a127a8515cfc909d7b73e991a55f9cc7d36f015a
CEK19 <nguyentrongnhan2001.work@gmail.com>
2023-10-05 07:18:26 +0700

    resizeable things

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,11 @@
 import React, { useEffect, useMemo, useState } from "react";
 import { Resizable } from "re-resizable";
-import useWindowDimensions from "@/hooks/useWindowDimensions";
 
 type Props = {
 	children: React.ReactNode;
 };
 
 const Layout = ({ children }: Props) => {
-	const [isOnClient, setIsOnClient] = useState(false);
-	const { width: widthScreen, height: heightScreen } = useWindowDimensions();
-	const defaultSize = useMemo(
-		() => ({ width: 120, height: heightScreen || 0 }),
-		[heightScreen]
-	);
 	const enableResizeConfig = {
 		top: false,
 		right: true,
@@ -24,23 +17,17 @@ const Layout = ({ children }: Props) => {
 		topLeft: false,
 	};
 
-	useEffect(() => {
-		setIsOnClient(true);
-	}, []);
-
 	return (
 		<React.Fragment>
-			{isOnClient && (
-				<div className="flex flex-row">
-					<Resizable
-						defaultSize={defaultSize}
-						enable={enableResizeConfig}
-					>
-						<div className="w-full h-full bg-red-600">Content</div>
-					</Resizable>
-					<div className="flex-1 resize-x bg-red-200">{children}</div>
-				</div>
-			)}
+			<div className="h-screen w-screen flex flex-row">
+				<Resizable
+					className="h-screen w-10"
+					enable={enableResizeConfig}
+				>
+					<div className="w-full h-full bg-red-600">Content</div>
+				</Resizable>
+				<div className="flex-1 resize-x bg-red-200">{children}</div>
+			</div>
 		</React.Fragment>
 	);
 };
